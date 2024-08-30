3541c88919dd89fed49883b46799cfe7cc7d8eb5
Tavleen Kaur <tavleenkaur3103@gmail.com>
2024-08-30 12:09:18 +0530

    changes done...

--- js/resume.js
+++ js/resume.js
@@ -1,7 +1,7 @@
 
   document.getElementById('downloadCvBtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'file/Tavleen_Resume.pdf'; // Update with the path to your CV file
+    link.href = 'file/Myresume.pdf'; // Update with the path to your CV file
     link.download = 'Tavleen_CV.pdf'; // The name for the downloaded file
     document.body.appendChild(link);
     link.click();
